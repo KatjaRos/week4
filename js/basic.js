c9dd08325a61711cc9ac563156a8554fb91c9745
Katja Rossi <katja.m.rossi@gmail.com>
2025-09-20 20:31:24 +0300

    yrityss 3  saada API-pyyntö toimimaan

--- js/basic.js
+++ js/basic.js
@@ -1,37 +1,39 @@
-const submitBtn = document.getElementById("submit-data")
-const input = document.getElementById("input-show")
-const container = document.querySelector(".show-container")
+document.addEventListener("DOMContentLoaded", () => {
+    const submitBtn = document.getElementById("submit-data")
+    const input = document.getElementById("input-show")
+    const container = document.querySelector(".show-container")
 
-submitBtn.addEventListener("click", async () => {
-    const query = input.value.trim()
-    container.innerHTML="";
-    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${encodeURIComponent(query)}`);
+    submitBtn.addEventListener("click", async () => {
+        const query = input.value.trim()
+        container.innerHTML="";
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${encodeURIComponent(query)}`);
 
-    const data = await response.json();
-    
-    data.forEach(item => {
-        const show = item.show;
-        const showDiv = document.createElement('div');
-        showDiv.classList.add('show-data');
-        const img = document.createElement('img');
-        img.src = show.image ? show.image.medium : "";
-        img.alt = show.name;
+        const data = await response.json();
+        
+        data.forEach(item => {
+            const show = item.show;
+            const showDiv = document.createElement('div');
+            showDiv.classList.add('show-data');
+            const img = document.createElement('img');
+            img.src = show.image ? show.image.medium : "";
+            img.alt = show.name;
 
-        const infoDiv = document.createElement('div')
-        infoDiv.classList.add('show-info');
+            const infoDiv = document.createElement('div')
+            infoDiv.classList.add('show-info');
 
-        const title = document.createElement('h1');
-        title.textContent = show.name;
+            const title = document.createElement('h1');
+            title.textContent = show.name;
 
-        const summary = document.createElement('div');
-        summary.innerHTML = show.summary 
+            const summary = document.createElement('div');
+            summary.innerHTML = show.summary 
 
-        infoDiv.appendChild(title);
-        infoDiv.appendChild(summary);
-        showDiv.appendChild(img);
-        showDiv.appendChild(infoDiv);
-        container.appendChild(showDiv);
+            infoDiv.appendChild(title);
+            infoDiv.appendChild(summary);
+            showDiv.appendChild(img);
+            showDiv.appendChild(infoDiv);
+            container.appendChild(showDiv);
+
+        });
 
     });
-
 });
